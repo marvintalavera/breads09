15cfee169a8fa5284bbd4acbb684572fdbad03c2
marvintalavera <talavera.marvin@rocketmail.com>
2023-04-22 11:43:56 -0700

    edit and update routes complete

--- views/show.jsx
+++ views/show.jsx
@@ -17,10 +17,17 @@ function Show ({bread, index}) {
                 have gluten.
             </p>
             <img src={bread.image} alt={bread.name} />
-            <li><a href="/breads">Go home</a></li>
+            <li>
+              <a href="/breads">Go home</a>
+            </li>
             <form action={`/breads/${index}?_method=DELETE`} method="POST">
               <input type='submit' value="DELETE"/>
             </form>
+            <a href={`/breads/${index}/edit`}>
+                <button>
+                  Edit
+                </button>
+            </a>
         </Default>
       )
   }
