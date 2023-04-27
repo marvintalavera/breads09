7af2e530becc210a1396bda9bd780b0e6e4dcba5
marvintalavera <talavera.marvin@rocketmail.com>
2023-04-26 20:49:01 -0700

    4.26 helper methods pt 1

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,12 +4,13 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-    res.render('Index',
-      {
-        breads: Bread
-      }
-    )
-  // res.send(Bread)
+  Bread.find()
+      .then(foundBreads => {
+          res.render('index', {
+              breads: foundBreads,
+              title: 'Index Page'
+          })
+      })
 })
 
 // NEW
@@ -19,28 +20,26 @@ breads.get('/new', (req, res) => {
 
 // CREATE
 breads.post('/', (req, res) => {
-  if (!req.body.image) {
-    req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+  if(!req.body.image) {
+      req.body.image = undefined 
   }
   if(req.body.hasGluten === 'on') {
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
   }
-  Bread.push(req.body)
+  Bread.create(req.body)
   res.redirect('/breads')
 })
 
 // SHOW
-breads.get('/:arrayIndex', (req, res) => {
-  if (Bread[req.params.arrayIndex]) {
-    res.render('Show', {
-      bread:Bread[req.params.arrayIndex],
-      index: req.params.arrayIndex,
-    })
-  } else {
-    res.render('404')
-  }
+breads.get('/:id', (req, res) => {
+  Bread.findById(req.params.id)
+      .then(foundBread => {
+          res.render('show', {
+              bread: foundBread
+          })
+      })
 })
 
 // EDIT
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // DEPENDENCIES
 const express = require('express')
 const methodOverride = require('method-override')
-
+const mongoose = require('mongoose')
 
 // CONFIGURATION
 require('dotenv').config()
@@ -29,6 +29,9 @@ app.use('/breads', breadsController)
 app.get('*', (req, res) => {
   res.send('404')
 })
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => { console.log('connected to mongo: ', process.env.MONGO_URI) })
   
 // LISTEN
 app.listen(PORT, () => {
